77a7fb3c5b6eb77debe888c3d2185a96b5ec7b35
ivan <ivanmorozoff30@gmail.com>
2021-09-20 15:05:45 +0300

    Разбор первой программы. [#159033 #205301]

--- src/main/java/ru/job4j/ru/job4j/ru/job4j/ru/job4j/ru/job4j/ru/job4j/Info.java
+++ src/main/java/ru/job4j/ru/job4j/ru/job4j/ru/job4j/ru/job4j/ru/job4j/Info.java
@@ -2,6 +2,3 @@ package ru.job4j.ru.job4j.ru.job4j.ru.job4j.ru.job4j.ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("19.09.2021");
-    }
-}
