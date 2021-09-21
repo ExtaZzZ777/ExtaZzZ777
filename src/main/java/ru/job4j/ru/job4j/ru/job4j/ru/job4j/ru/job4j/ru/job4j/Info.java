441aa40386ceb107cfc7686d4afa1223b3bbd0bc
ivan <ivanmorozoff30@gmail.com>
2021-09-21 14:53:41 +0300

    Арифметические операции. [#185]

--- src/main/java/ru/job4j/ru/job4j/ru/job4j/ru/job4j/ru/job4j/ru/job4j/Info.java
+++ src/main/java/ru/job4j/ru/job4j/ru/job4j/ru/job4j/ru/job4j/ru/job4j/Info.java
@@ -2,3 +2,21 @@ package ru.job4j.ru.job4j.ru.job4j.ru.job4j.ru.job4j.ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
+        byte age = 19;
+        System.out.println(age);
+        char floor = 'Y';
+        System.out.println(floor);
+        float height = 1.95F;
+        System.out.println(height);
+        int stones = 50000;
+        System.out.println(stones);
+        long bacteria = 900500;
+        System.out.println(bacteria);
+        double distance = 100.000;
+        System.out.println(distance);
+        boolean permission = true;
+        System.out.println(permission);
+        int size = 100500;
+        System.out.println(size);
+    }
+}
